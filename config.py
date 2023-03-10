3d5ec8e921a56c3b8935c6d3fcd7974df3991a0e
ɪᴍ_ᴘʀᴏᴛʀɪᴄᴋs <115210962+Protricks@users.noreply.github.com>
2023-03-10 16:06:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DevilsHeavenMF")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/lovelychats01")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
